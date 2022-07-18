13478f93024e2061325f632e4068a6e853a9ddd8
Rami Ibrahim <rtamimi78@gmail.com>
2022-07-18 08:12:59 -0400

    Add files via upload

---
+++ scam/keras.py
@@ -0,0 +1,56 @@
+from keras import Model
+import numpy as np
+
+from scam.exceptions import InvalidState
+from scam.utils import resize_activations, normalize_activations
+
+
+class ScoreCAM:
+    def __init__(self, model_input, last_conv_output, softmax_output, input_shape, cam_batch_size=None):
+        """
+        Prepares class activation mappings
+        :param model_input: input layer of CNN, normally takes batch of images as an input. Currently batch must be limited to a single image
+        :param last_conv_output: last convolutional layer. The last conv layer contains the most complete information about image.
+        :param softmax_output: flat softmax (or similar) layer describing the class certainty
+        :param input_shape: Expecting a batch of a single input sample 1 x M X N X ...; it is assumed that 2D image of M x N dimensions is served as an input, which can be multiplied with a 2D-mask.
+        :param cam_batch_size: Optional, defaults to None, which will result in inference of batches of size 32.
+        """
+        self.model_input = model_input
+        self.last_conv_output = last_conv_output
+        self.softmax_output = softmax_output
+        self.last_conv_model = Model(inputs=model_input, outputs=last_conv_output)
+        self.softmax_model = Model(inputs=model_input, outputs=softmax_output)
+        self.input_shape = input_shape
+        self.cam_batch_size = cam_batch_size
+
+        self.normalized_maps = None
+        self.classes_activation_scale = None
+
+    def prepare_cam(self, input):
+        output_conv = self.last_conv_model.predict(input)
+        # Only first image from convolutions will be used
+        resized = resize_activations(output_conv[0], self.input_shape)
+        # filter_size x input_shape[0] x input_shape[1] - resized to original input dimensions
+        normalized_maps = normalize_activations(resized)
+
+        # repeat input
+        repeat_input = np.tile(input, (normalized_maps.shape[0], 1, 1, 1))
+        expanded_activation_maps = np.expand_dims(normalized_maps, axis=3)
+        masked_images = np.multiply(repeat_input, expanded_activation_maps)
+        # input: filter_size x input_shape[0] x input_shape[1] -> Output filter_size x Classes_Count
+        self.classes_activation_scale = self.softmax_model.predict(masked_images,
+                                                                   batch_size=self.cam_batch_size)
+        self.normalized_maps = normalized_maps
+        #return 3D array
+        return self.normalized_maps
+
+    def get_class_heatmap(self, class_id):
+        if self.normalized_maps is None or self.classes_activation_scale is None:
+            raise InvalidState('Call prepare_cam before accessing get_class_heatmap, '
+                               'activations must be prepared via prepare_cam')
+        final_weights = self.classes_activation_scale[:, class_id]
+        final_maps = np.multiply(self.normalized_maps, final_weights.reshape((-1, 1, 1)))
+        # ReLU
+        final_maps_max = np.max(final_maps, axis=0)
+        final_class_activation_map = np.where(final_maps_max > 0, final_maps_max, 0)
+        return final_class_activation_map
